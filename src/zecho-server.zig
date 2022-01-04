f858fa926b2fc8ea8345b1f99c8b6f27afac35e8
Ian Applegate <ian@jedi.sh>
2022-01-04 01:02:08 -0500

    refactor, fix most issues

--- src/zecho-server.zig
+++ src/zecho-server.zig
@@ -4,7 +4,7 @@ const builtin = @import("builtin");
 const net = std.net;
 const os = std.os;
 const io = std.io;
-const ArrayList = std.ArrayList;
+const AutoArrayHashMapUnmanaged = std.AutoArrayHashMapUnmanaged;
 const log = std.log.scoped(.zecho);
 
 const c = @cImport({
@@ -46,189 +46,191 @@ pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     var gpa = general_purpose_allocator.allocator();
 
-    var server = Zecho().init(gpa);
+    var server = Zecho.init(gpa);
     defer server.deinit();
     try server.parseArgs();
     try server.start();
 }
 
-pub fn Zecho() type {
-    return struct {
-        const Self = @This();
+const Zecho = struct {
+    const Self = @This();
 
-        allocator: std.mem.Allocator,
-        arg_address: [:0]const u8,
-        arg_port: u16,
-        arg_count: u16,
-        arg_udp: bool,
-        arg_uring: bool,
+    allocator: std.mem.Allocator,
+    arg_address: [:0]const u8,
+    arg_port: u16,
+    arg_count: u16,
+    arg_udp: bool,
+    arg_uring: bool,
 
-        parsed_address: std.net.Address,
-        found_port: bool,
-        found_address: bool,
-        barrier: Barrier,
-        socket: os.socket_t,
-        socket_len: os.socklen_t,
-        clients: ArrayList(*Client),
-        tick: u64,
-        mutex: std.Thread.Mutex,
+    parsed_address: std.net.Address,
+    found_port: bool,
+    found_address: bool,
 
-        pub fn init(allocator: std.mem.Allocator) Self {
-            return Self {
-                .allocator = allocator,
-                .arg_address = undefined,
-                .arg_port = undefined,
-                .arg_count = 0,
-                .arg_udp = false,
-                .arg_uring = false,
-                .parsed_address = undefined,
-                .found_port = false,
-                .found_address = false,
-                .barrier = undefined,
-                .socket = undefined,
-                .socket_len = 0,
-                .clients = ArrayList(*Client).init(allocator),
-                .tick = 0,
-                .mutex = std.Thread.Mutex{},
-            };
-        }
+    barrier: Barrier,
+    socket: os.socket_t,
+    socket_len: os.socklen_t,
+    clients: std.AutoArrayHashMapUnmanaged(i32, *Client),
+    tick: u64,
+    mutex: std.Thread.Mutex,
+    
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self {
+            .allocator = allocator,
+            .arg_address = undefined,
+            .arg_port = undefined,
+            .arg_count = 0,
+            .arg_udp = false,
+            .arg_uring = false,
+            .parsed_address = undefined,
+            .found_port = false,
+            .found_address = false,
+            .barrier = undefined,
+            .socket = undefined,
+            .socket_len = 0,
+            .clients = .{},
+            .tick = 0,
+            .mutex = std.Thread.Mutex{},
+        };
+    }
 
-        pub fn start(self: *Self) !void {
-            const kernel_backlog = 1;
+    pub fn start(self: *Self) !void {
+        const kernel_backlog = 1;
 
-            self.socket = try os.socket(self.parsed_address.any.family, os.SOCK.STREAM | os.SOCK.CLOEXEC | os.SOCK.NONBLOCK, os.IPPROTO.TCP);
-            self.socket_len = self.parsed_address.getOsSockLen();
+        self.socket = try os.socket(self.parsed_address.any.family, os.SOCK.STREAM | os.SOCK.CLOEXEC | os.SOCK.NONBLOCK, os.IPPROTO.TCP);
+        self.socket_len = self.parsed_address.getOsSockLen();
 
-            try os.setsockopt(self.socket, os.SOL.SOCKET, os.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
-            try os.bind(self.socket, &self.parsed_address.any, self.socket_len);
-            try os.listen(self.socket, kernel_backlog);
+        try os.setsockopt(self.socket, os.SOL.SOCKET, os.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+        try os.bind(self.socket, &self.parsed_address.any, self.socket_len);
+        try os.listen(self.socket, kernel_backlog);
 
-            self.barrier = Barrier{};
-            self.barrier.start();
+        self.barrier = Barrier{};
+        self.barrier.start();
 
-            // Cleanup has a race condition, so this will leak the thread handles until fixed
-            //_ = std.Thread.spawn(.{}, cleanup, .{ self }) catch unreachable;
-
-            while(!is_exiting) {
-                std.os.nanosleep(0, 10);
-                const peer = os.accept(self.socket, &self.parsed_address.any, &self.socket_len, os.SOCK.CLOEXEC | os.SOCK.NONBLOCK) catch continue;
-                if(@TypeOf(peer) == i32) {
-                    var client_ptr = self.allocator.create(Client) catch unreachable;
-                    client_ptr.* = client_ptr.init();
-                    client_ptr.* = client_ptr.add(peer);
-                    self.mutex.lock();
-                    try self.clients.append(client_ptr);
-                    self.mutex.unlock();
-                }
+        var last_cleanup = std.time.milliTimestamp();
+        while(!is_exiting) {
+            if((std.time.milliTimestamp() - last_cleanup) > 5000) {
+                self.mutex.lock();
+                cleanup(self);
+                self.mutex.unlock();
+                last_cleanup = std.time.milliTimestamp();
             }
-            self.barrier.stop();
-        }
-
-        pub fn deinit(self: *Self) void {
-            os.close(self.socket);
-            self.clients.deinit();
-        }
-
-        inline fn remove(self: *Self, sock: *Client) void {
-            for (self.clients.items) |client, i| {
-                if (sock.fd == client.fd) {
-                    _ = self.clients.swapRemove(i);
-                }
+            std.os.nanosleep(0, 0);
+            const peer = os.accept(self.socket, &self.parsed_address.any, &self.socket_len, os.SOCK.CLOEXEC | os.SOCK.NONBLOCK) catch continue;
+            if(@TypeOf(peer) == i32) {
+                var client_ptr = self.allocator.create(Client) catch unreachable;
+                client_ptr.* = client_ptr.init();
+                client_ptr.* = client_ptr.add(self, peer);
+                self.mutex.lock();
+                try self.clients.put(self.allocator, peer, client_ptr);
+                self.mutex.unlock();
             }
         }
+        self.barrier.stop();
+    }
 
-        pub fn cleanup(self: *Self) void {
-            while(self.barrier.isRunning()) {
-                for (self.clients.items) |client| {
-                    if(client.dead) {
-                        self.mutex.lock();
-                        remove(self, client);
-                        self.mutex.unlock();
-                    }
-                }
-                std.os.nanosleep(1, 0);
+    pub fn deinit(self: *Self) void {
+        os.close(self.socket);
+        self.clients.deinit(self.allocator);
+    }
+
+    pub fn cleanup(self: *Self) void {
+        var it = self.clients.iterator();
+
+        var i: usize = 0;
+        var clean: usize = 0;
+        var honored: std.ArrayList(i32) = std.ArrayList(i32).init(self.allocator);
+        while (it.next()) |client| : (i += 1) {
+            if(client.value_ptr.*.dead) {
+                var fd = client.value_ptr.*.fd;
+                self.allocator.destroy(self.clients.getPtr(fd).?.*);
+                honored.append(fd) catch undefined;
             }
         }
+        for(honored.items) |fd| {
+            _ = self.clients.swapRemove(fd);
+            os.close(fd);
+            clean += 1;
+        }
+    }
 
-        pub fn parseArgs(self: *Self) anyerror!void {
-            const argv: [][*:0]const u8 = os.argv;
-            const result = flags.parse(argv[1..], &[_]flags.Flag{
-                .{ .name = "--help", .kind = .boolean },
-                .{ .name = "-h", .kind = .boolean },
-                .{ .name = "--version", .kind = .boolean },
-                .{ .name = "-v", .kind = .boolean },
-                .{ .name = "-u", .kind = .boolean },
-                .{ .name = "--udp", .kind = .boolean },
-                .{ .name = "-p", .kind = .arg },
-                .{ .name = "-a", .kind = .arg },
-                .{ .name = "-c", .kind = .arg },                
-            }) catch {
-                try io.getStdErr().writeAll(usage);
+    pub fn parseArgs(self: *Self) anyerror!void {
+        const argv: [][*:0]const u8 = os.argv;
+        const result = flags.parse(argv[1..], &[_]flags.Flag{
+            .{ .name = "--help", .kind = .boolean },
+            .{ .name = "-h", .kind = .boolean },
+            .{ .name = "--version", .kind = .boolean },
+            .{ .name = "-v", .kind = .boolean },
+            .{ .name = "-u", .kind = .boolean },
+            .{ .name = "--udp", .kind = .boolean },
+            .{ .name = "-p", .kind = .arg },
+            .{ .name = "-a", .kind = .arg },
+            .{ .name = "-c", .kind = .arg },                
+        }) catch {
+            try io.getStdErr().writeAll(usage);
+            os.exit(1);
+        };
+        if (result.boolFlag("--help") or result.boolFlag("-h")) {
+            try io.getStdOut().writeAll(usage);
+            os.exit(0);
+        }
+        if (result.args.len != 0) {
+            std.log.err("unknown option '{s}'", .{result.args[0]});
+            try io.getStdErr().writeAll(usage);
+            os.exit(1);
+        }
+        if (result.boolFlag("--version") or result.boolFlag("-v")) {
+            try io.getStdOut().writeAll(build_version);
+            os.exit(0);
+        }
+        if (result.argFlag("-a")) |address| {
+            if(result.args.len == 0) {
+                log.info("Found ip address: {s}", .{address});
+                self.arg_address = std.mem.span(address);
+                self.found_address = true;
+            } else {
+                try io.getStdErr().writeAll("Invalid argument for -a expected type [u8]\n");
                 os.exit(1);
+            }
+        }
+        if (result.argFlag("-p")) |port| {
+            const maybe_port = std.fmt.parseInt(u16,  std.mem.span(port), 10) catch null;
+            if(maybe_port) |int_port| {
+                log.info("Found port address: {}", .{int_port});
+                self.arg_port = int_port;
+                self.found_port = true;
+            } else {
+                try io.getStdErr().writeAll("Invalid argument for -p expected type [u16]\n");
+                os.exit(1);            
+            }
+        }
+        if (result.argFlag("-c")) |count| {
+            const maybe_count = std.fmt.parseInt(u16,  std.mem.span(count), 10) catch null;
+            if (maybe_count) |int_count| {
+                log.info("Found int count: {}", .{int_count});
+                self.arg_count = int_count;
+            } else {
+                try io.getStdErr().writeAll("Invalid argument for -c expected type [u16]\n");
+                os.exit(1);
+            }
+        }
+        if(self.found_address and self.found_port) {
+            const arg_con = net.Address.parseIp(self.arg_address, self.arg_port) catch {
+                    try io.getStdErr().writeAll("Invalid address and/or port.\n");
+                    os.exit(1);        
             };
-            if (result.boolFlag("--help") or result.boolFlag("-h")) {
+            log.info("Found valid port & address.", .{});
+            self.parsed_address = arg_con;
+        } else {
+            if (os.argv.len - 1 == 0) {
                 try io.getStdOut().writeAll(usage);
                 os.exit(0);
-            }
-            if (result.args.len != 0) {
-                std.log.err("unknown option '{s}'", .{result.args[0]});
-                try io.getStdErr().writeAll(usage);
-                os.exit(1);
-            }
-            if (result.boolFlag("--version") or result.boolFlag("-v")) {
-                try io.getStdOut().writeAll(build_version);
-                os.exit(0);
-            }
-            if (result.argFlag("-a")) |address| {
-                if(result.args.len == 0) {
-                    log.info("Found ip address: {s}", .{address});
-                    self.arg_address = std.mem.span(address);
-                    self.found_address = true;
-                } else {
-                    try io.getStdErr().writeAll("Invalid argument for -a expected type [u8]\n");
-                    os.exit(1);
-                }
-            }
-            if (result.argFlag("-p")) |port| {
-                const maybe_port = std.fmt.parseInt(u16,  std.mem.span(port), 10) catch null;
-                if(maybe_port) |int_port| {
-                    log.info("Found port address: {}", .{int_port});
-                    self.arg_port = int_port;
-                    self.found_port = true;
-                } else {
-                    try io.getStdErr().writeAll("Invalid argument for -p expected type [u16]\n");
-                    os.exit(1);            
-                }
-            }
-            if (result.argFlag("-c")) |count| {
-                const maybe_count = std.fmt.parseInt(u16,  std.mem.span(count), 10) catch null;
-                if (maybe_count) |int_count| {
-                    log.info("Found int count: {}", .{int_count});
-                    self.arg_count = int_count;
-                } else {
-                    try io.getStdErr().writeAll("Invalid argument for -c expected type [u16]\n");
-                    os.exit(1);
-                }
-            }
-            if(self.found_address and self.found_port) {
-                const arg_con = net.Address.parseIp(self.arg_address, self.arg_port) catch {
-                        try io.getStdErr().writeAll("Invalid address and/or port.\n");
-                        os.exit(1);        
-                };
-                log.info("Found valid port & address.", .{});
-                self.parsed_address = arg_con;
             } else {
-                if (os.argv.len - 1 == 0) {
-                    try io.getStdOut().writeAll(usage);
-                    os.exit(0);
-                } else {
-                    try io.getStdErr().writeAll("Address and/or port not found.\n");
-                    os.exit(1);            
-                }
+                try io.getStdErr().writeAll("Address and/or port not found.\n");
+                os.exit(1);            
             }
         }
-    };
-}
+    }
+};
 
 fn handleInterrupt(signal: c_int) callconv(.C) void {
     is_exiting = true;
@@ -244,6 +246,7 @@ const Client = struct {
     ts: i64,
     timeout: i64,
     dead: bool,
+    owner: *Zecho,
 
     pub fn init(self: *Self) Self {
         _ = self;
@@ -253,16 +256,18 @@ const Client = struct {
             .ts = undefined,
             .timeout = undefined,
             .dead = undefined,
+            .owner = undefined,
         };
     }
 
-    pub fn add(self: *Self, fd: i32) Self {
+    pub fn add(self: *Self, owner: *Zecho, fd: i32) Self {
         return Self {
             .fd = fd,
             .thread = std.Thread.spawn(.{}, run, .{ self }) catch unreachable,
             .ts = std.time.milliTimestamp(),
             .timeout = 5000,
             .dead = false,
+            .owner = owner,
         };
     }
 
@@ -272,14 +277,13 @@ const Client = struct {
         while(!is_exiting) {
             if((std.time.milliTimestamp() - self.ts) > self.timeout) {
                 self.dead = true;
-                os.close(self.fd);
                 break;
             }
             
             const bytes_read = read_(self.fd, &recv_buf) catch continue;
 
             if(bytes_read > 0) {
-                const bytes_written = os.linux.write(self.fd, &recv_buf, recv_buf.len);
+                const bytes_written = os.write(self.fd, &recv_buf) catch continue;
                 _ = bytes_written;
                 recv_buf = undefined;
                 self.ts = std.time.milliTimestamp();
